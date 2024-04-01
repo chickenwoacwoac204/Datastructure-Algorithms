9e8f447599464835d0fdb694115a570a2da7a671
chickenwoacwoac204 <thanhthao2041104@gmail.com>
2024-04-01 10:33:57 +0700

    day la tim so fibbonacci thu n

---
+++ fibbonacci.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int fib(int n) {
+	if (n==0 || n==1) {
+		return 1;
+	}
+	return fib(n-1) + fib(n-2);
+}
+
+int main() {
+	int n;
+	cout << "Nhap n: ";
+	cin >> n;
+	cout << "So Fibbonacci thu n la: " << fib(n);
+}
